9944dacc52fe02731aa401aa55f51744fe75537a
doc <doc@coveycs.com>
2015-09-30 01:54:58 +0000

    Miscellaneous changes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -24,13 +24,13 @@ class Navigation {
             <NavItem eventKey={2} href='/downloads'>Download</NavItem>
             <NavItem eventKey={3} href='/rules'>Rules</NavItem>
             <NavItem eventKey={4} href='/faq'>FAQ</NavItem>
-            <NavItem eventKey={5} href='/about' onClick={Link.handleClick}>About</NavItem>
+            <NavItem eventKey={5} href='http://forum.trading.football'>Forum</NavItem>
+            <NavItem eventKey={6} href='/about' onClick={Link.handleClick}>About</NavItem>
           </Nav>
         </Navbar>
       </div>
     );
   }
-
 }
 
 export default Navigation;
