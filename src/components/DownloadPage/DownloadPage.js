70d7f88a6379a3b29bdfd74809899cc3628cbeec
Jay Berg <jaybny@gmail.com>
2015-12-15 00:26:35 +0000

    Did the copy for About, Download, FAQ, and Rules

--- src/components/DownloadPage/DownloadPage.js
+++ src/components/DownloadPage/DownloadPage.js
@@ -144,12 +144,35 @@ class DownloadPage extends React.Component{
       <div className="DownloadPage">
         <div className="DownloadPage-container">
           <PageHeading text={text} logoSize='sm' />
-          {this.getOSContent()}
           <div>
-            <h3>Support</h3>
-            <p className='backwards'>moc.ysatnafihsotas@ofni</p>
-            <p>Hotline: 650-822-2777</p>
+
+		  
+
+<h2>Become a Trader on Trading.Football</h2>
+
+<p>Any fantasy football player can be a Trader on Trading.Football, but the most skilled Traders  represent the best-of-the-best in fantasy expertise. Traders are the foundation of the Trading.Football community and power what is the world’s first prediction market for fantasy football results. By playing Trading.Football, Traders are able to demonstrate their raw fantasy skills to the world.</p>
+
+<h3>How To Play</h3>
+<p>Traders play Trading.Football by making projections on NFL player’s fantasy results. Traders are able to make projections for every offensive player - QB, RB, WR, TE, K.  Based on the accuracy of their projections, Traders are rewarded tokens in the form of FantasyBits.</p>
+
+<p>Traders then use FantasyBits to trade NFL players on Trading.Football’s gamified exchange, buying when a player is undervalued and selling when a player is overvalued in attempt to profit and earn more FantasyBits. The act of buying and selling with profit and loss creates an efficient market, which generates each NFL player’s price that reflects all relevant information. Player prices, stats and charts generated from Trading.Football’s Traders are displayed in a streaming FantasyTicker that users can follow to receive the most accurate, market-driven intel in the fantasy football industry.</p>
+
+<p>FantasyBits are rare, with 100 FantasyBits minted for every fantasy point scored each week by NFL players. FantasyBits can only be earned with raw fantasy football skills, which is determined by Trading.Football’s groundbreaking “Proof-of-Skill” consensus algorithm. FantasyBits act solely as tokens on the Trading.Football platform and cannot be traded for real currency.</p>
+
+<h3>How to Get Started</h3>
+<p>Simply download the Trading.Football desktop program for Windows or Mac from the links below to get started and showcase your true fantasy skills to the world!</p>
+		  
           </div>
+		  
+          {this.getOSContent()}
+		  
+		  <div>
+		  
+<h3>Support</h3>
+<p>Email: <a href='mailto://info@satoshifantasy.com'>info@satoshifantasy.com</a></p>
+<p>Hotline: 650-822-2777</p>
+
+		  </div>
         </div>
       </div>
     );
